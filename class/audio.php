6d053b91a0a68635ec68164d8bfcea6522c0fa98
unknown <Pascal@.(none)>
2011-04-09 13:31:40 +0200

    Amélioration de la classe audio
    Générer un fichier mp3 pour chaque ligne + création du fichier complet.
    
    Signed-off-by: unknown <Pascal@.(none)>

--- class/audio.php
+++ class/audio.php
@@ -8,10 +8,10 @@ class audio
 	{
 		$this->lang=$lang;
 	}
-	public function creation_audio($string)
+	public function creation_audio($string,$id)
 	{
 		$ch = curl_init();
-		$options = array(   CURLOPT_URL => 'http://translate.google.fr/translate_tts?ie=UTF-8&q='.$string.'&tl='.$this->lang,
+		$options = array(   CURLOPT_URL => 'http://translate.google.fr/translate_tts?ie=UTF-8&q='.urlencode($string).'&tl='.$this->lang,
                         CURLOPT_RETURNTRANSFER => true,
                         CURLOPT_SSLVERSION => 3,
                            CURLOPT_SSL_VERIFYPEER => false,
@@ -22,22 +22,45 @@ class audio
 		$output = curl_exec($ch);
 		if(curl_errno($ch)) {  echo 'Erreur Curl : ' . curl_error($ch); }
 		curl_close($ch);
-		$Fnm = "audio.mp3";
+		$Fnm = $id.".mp3";
 		$inF = fopen($Fnm,"w");
 		fputs($inF,$output);
 		fclose($inF);
-		$handle = fopen('audio.mp3', "rb");
-		$size = filesize('audio.mp3');
-		$load = stream_get_line($handle, $size);
-		fclose($handle);
-		$tab=array("mp3" => $load, "size" => $size);
-		$this->audio.=$tab['mp3'];
 	}
-	public function audiofinal()
+	public function GetAudio($file) 
 	{
+		$file = strtolower($file);
+		$handle = fopen($file, "rb");
+		$size = filesize($file);
+		if (function_exists('stream_get_line')) 
+		{
+			$load = stream_get_line($handle, $size);
+		}
+		else
+		{
+			$load = fread($handle, $size);
+		}
+		fclose($handle);
+		return array("mp3" => $load, "size" => $size);
+	}
+	public function audiofinal($nb_ligne)
+	{
+		$audio=array();
+		for($i=1;$i<=$nb_ligne; $i++)
+		{
+			$audio[]=$this->GetAudio($i.".mp3");
+			unlink($i.".mp3");
+		}
+		$mp3='';
+		$size='';
+		foreach ($audio as $key => $value) 
+		{
+			$mp3 .= $audio[$key]['mp3'];
+			$size .= $audio[$key]['size'];
+		}
 		$Fnm = "final.mp3";
 		$inF = fopen($Fnm,"w");
-		fputs($inF,$this->audio);
+		fputs($inF,$mp3);
 		fclose($inF);
 	}
 }
