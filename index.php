1ef6934a30f8371fac5c08cdae68aee734fd30e5
unknown <Pascal@.(none)>
2011-04-09 12:48:51 +0200

    création de l'instance audio
    Signed-off-by: unknown <Pascal@.(none)>

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 ﻿<?php
 include('class/parser.php');
 include('class/audio.php');
-
 /*
     $string = urlencode("Bienvenu à la Mairie de Mirande");
     $ch = curl_init();
@@ -17,17 +16,18 @@ include('class/audio.php');
     $output = curl_exec($ch);
     if(curl_errno($ch)) {  echo 'Erreur Curl : ' . curl_error($ch); }
     curl_close($ch);
-    $Fnm = "aide.mp3";
-    $inF = fopen($Fnm,"w");
-    fputs($inF,$output);
-    fclose($inF);*/
+    */
 $chaine="Une interface utilisateur WYSIWYG permet de composer visuellement le résultat voulu, typiquement pour un logiciel de mise en page, un traitement de texte ou d'image. C'est une interface \" intuitive \": l'utilisateur voit directement à l'écran à quoi ressemblera le résultat final. Cette interface permet, à des utilisateurs ne connaissant aucun langage de programmation de pouvoir mettre en forme le texte et les images de son choix.
 Dans ce tutoriel, vous apprendrez à télécharger le système complet et à configurer une page basique, présentant un textarea. Grâce à jquery, le textarea sera remplacé par le système wysiwyg.";
 $parsing = new parser();
+$audio=new audio();
 $newtext = $parsing->coupe($chaine);
-echo "<pre>";
-print_r($newtext);
-echo "</pre>";
+foreach($newtext as $t)
+{
+	echo $t.'<br />';
+	$audio->creation_audio($t);
+}
+$audio->audiofinal();
 ?><!-- 
 <object type="application/x-shockwave-flash" data="dewplayer-mini.swf?mp3=aide.mp3" width="303" height=113" id="dewplayer-mini">
 <param name="wmode" value="transparent" /><param name="movie" value="dewplayer-mini.swf?mp3=aide.mp3" />
