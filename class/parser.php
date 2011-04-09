e3eff80717a8a8231acf0408be84e063e1c7ea74
unknown <Pascal@.(none)>
2011-04-09 15:20:11 +0200

    Amélioration de la génération du texte à créer
    Encore des modifications
    
    Signed-off-by: leknoppix <Pascal@.(none)>

--- class/parser.php
+++ class/parser.php
@@ -7,8 +7,39 @@ class parser
 	public function coupe($chaine)
 	{
 		$this->chaine=str_replace('\n','',strip_tags(nl2br($chaine)));
-		$this->chaine = wordwrap( $this->chaine, 60 , "\n" );
-		$this->resultat = explode("\n", $this->chaine);
+		$this->chaine=str_replace('.','./point/',$this->chaine);
+		$this->chaine=str_replace(',',',/virgule/',$this->chaine);
+		$this->chaine=str_replace('!','!/exclamation/',$this->chaine);
+		$this->chaine = explode('/point/', $this->chaine);
+		$nouvellechaine='';
+		foreach($this->chaine as $v)
+		{
+			$pointexclamation=explode('/exclamation/',$v);
+			foreach($pointexclamation as $o)
+			{
+				if(strlen($o)>'150')
+				{
+					$toto=explode("/virgule/", $o);
+					foreach($toto as $k)
+					{
+						if(strlen($k)>'150')
+						{
+							$nouvellechaine.=wordwrap( $k, 150 , "\n" );
+						}
+						else
+						{
+							$nouvellechaine.=$k."\n";
+						}
+					}
+				}
+				else
+				{
+					$nouvellechaine.=$o."\n";
+				}
+			}
+		}
+		$nouvellechaine=str_replace('/virgule/','',$nouvellechaine);
+		$this->resultat = explode("\n", $nouvellechaine);
 		return $this->resultat;
 	}
 }
