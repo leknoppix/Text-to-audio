415e3abe7c9fa6f5bfafdc3ebddce6ee576a18c9
unknown <Pascal@.(none)>
2011-04-09 13:36:09 +0200

    modification diverse
    modification diverse
    
    Signed-off-by: unknown <Pascal@.(none)>

--- index.php
+++ index.php
@@ -17,14 +17,11 @@ include('class/audio.php');
     if(curl_errno($ch)) {  echo 'Erreur Curl : ' . curl_error($ch); }
     curl_close($ch);
     */
-$chaine="Sous la baguette de maestro Max comme le présente JP Pierre CAUBERE,   Président et musicien  de la sté Philharmonique de Mirande ,  maire adjoint. En ce 1er Avril comme dans un aquarium, le cœur des enfants de l’école de musique mirandaise accompagné de l’orchestre de la Sté Philharmonique s’est une fois de plus démarquée.  Sous le regard de plus de 900 spectateurs conquis, ils ont interprété 12 morceaux emblématiques d’un siècle  de chanson française, thématique choisie et insufflée dans le programme pédagogique des écoliers mirandais par Max Fouga, Directeur de l’école de musique municipale. Le concert du 1er avril 2011 à la salle André Beaudran décorée des réalisations artistiques de nos têtes blondes mirandaises restera un moment inoubliable. Un exercice brillant qui réunit  près de  55 musiciens et 250 enfants des écoles primaires publiques et privées, présenté et interprété par, on y croyait, un véritable titi parisien paré de son haut de forme, sa queue de pie et ses gants blancs Michel accompagné de Mistinguette en personne, tant la voix et les effets de Rose sont convaincants.
-
-Un succès mérité car 6 mois de travail, d’écriture, de répétition, de montage,  furent nécessaires pour un tel résultat. De la réécriture des partitions pour instruments à vent, au montage vidéo diffusé pendant la rétrospective du présentateur , en passant par la réalisation des programmes et de la communication, Max Fouga  a tout fait , aidé il faut toutefois le souligné des personnels éducatif et territorial pour certains aspects.
-
-Un grand merci à tous les intervenants et encore félicitations à vous Maestro, aux musiciens et chanteurs pour ce moment de pur bonheur qui plus est gratuit (seul le programme décoré par les écoliers était vendu à la discrétion des spectateurs).
-
-Un événement professionnel que l’on souhaite à nouveau vivre pourquoi pas à l’occasion de la fête de la musique mirandaise….
-";
+$chaine="Blogs, réseaux sociaux, pages d'accueil personnalisables... Depuis quelques années, les sites web ont gagné en fonctionnalités et sont devenus dans le même temps de plus en plus complexes.
+Que le temps de la \"page web perso\" est loin ! Il y a une époque où l'on pouvait se contenter de créer un site basique. Un peu de texte, quelques images : hop là, notre site perso était prêt. :-°
+Aujourd'hui, c'est différent : il faut que ça bouge ! On s'attend à ce qu'un site soit régulièrement mis à jour : on veut voir des actualités sur la page d'accueil, on veut pouvoir les commenter, discuter sur des forums, bref, participer à la vie du site.
+Le langage PHP a justement été conçu pour créer des sites \"vivants\" (on parle de sites dynamiques). Et si vous voulez apprendre à créer vous aussi des sites web dynamiques, c'est votre jour de chance : vous êtes sur un cours pour vrais débutants en PHP !
+L'essentiel, c'est de lire en entier les chapitres dans l'ordre. Après, ça passe tout seul et vous vous étonnerez bientôt de ce que vous êtes capable de faire ! ";
 $parsing = new parser();
 $audio=new audio();
 $newtext = $parsing->coupe($chaine);
